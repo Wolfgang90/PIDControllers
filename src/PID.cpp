183250cecbe62def12b3eccba058bf9fa79eb7d7
Wolfgang90 <eichler_wolfgang@msn.com>
2017-05-23 17:12:23 +0200

    Initialized PID in class call

--- src/PID.cpp
+++ src/PID.cpp
@@ -6,12 +6,11 @@ using namespace std;
 * TODO: Complete the PID class.
 */
 
-PID::PID() {}
+PID::PID(double Kp, double Ki, double Kd): Kp(Kp), Ki(Ki), Kd(Kd), p_error(0), i_error(0), d_error(0){
+}
 
 PID::~PID() {}
 
-void PID::Init(double Kp, double Ki, double Kd) {
-}
 
 void PID::UpdateError(double cte) {
 }
--- src/main.cpp
+++ src/main.cpp
@@ -32,7 +32,7 @@ int main()
 {
   uWS::Hub h;
 
-  PID pid;
+  PID pid = PID(1.0,0.0,0.0);
   // TODO: Initialize the pid variable.
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
